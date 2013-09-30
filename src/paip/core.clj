9bf12c01a035d05091593ab6e41a6ef3cfb68be0
Johnvp <johnvp@MacOSXNew-Change-the-Hostname-2.local>
2013-09-30 11:54:44 +0530

    Basic GPS added

--- src/paip/core.clj
+++ src/paip/core.clj
@@ -1,6 +1,39 @@
-(ns paip.core)
+(ns paip.core
+  (:use [clojure.set]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn member? 
+  "true if seq contains elm"
+  [elem seq]  
+  (some #(= elem %) seq))
+
+(defn as-atom [xs]
+  (atom (into #{} xs)))
+
+(def ^:dynamic state "current state of GPS" (atom #{}))
+
+(def ^:dynamic ops "list of operations" (atom '()))
+
+(defrecord Op [action preconds add-list del-list])
+
+
+(defn GPS "General propblem solver, achive all goals using ops" [current-state goals operations]
+  (binding [state (as-atom current-state) ops (as-atom operations)]
+    (if (every? achieve goals) "solved")))
+  
+
+(defn achieve [goal]
+  (or (member? goal @state)
+      (some apply-op! (filter #(appropriate? goal %1) @ops))))
+
+(defn appropriate? "An op is appropriate to a goal if it is in its add-list" [goal op]
+  (member? goal (:add-list op)))
+
+
+(defn apply-op! [op]
+  (if (every? achieve (:preconds op))
+    (do 
+      (println "executing " (:action op))
+      (reset! state (difference @state (:del-list op)))
+      (reset! state (union @state (:add-list op)))
+      true)
+    false))
